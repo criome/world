50de61c59178954d5f93ad6a279666bb64cd4739
li <li@maisiliym.criome.me>
2025-03-07 14:18:26 +0100

    added[horizons typed-atom]

--- flake.nix
+++ flake.nix
@@ -4,12 +4,18 @@
   inputs = {
     atom.url = "github:LiGoldragon/atom";
 
+    typed-atom.url = "github:criome/typed-atom";
+    typed-atom.inputs.rust-atom.follows = "rust-atom";
+    rust-atom.url = "github:criome/rust-atom";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-atom.url = "github:criome/nixpkgs-atom";
     nixpkgs-atom.inputs.nixpkgs.follows = "nixpkgs";
 
     pkdjz.url = "github:criome/pkdjz";
     pkdjz.inputs.nixpkgs.follows = "nixpkgs";
+
+    horizons.url = "github:criome/horizons";
 
     liGoldragonWebpage.url = "github:LiGoldragon/webpage";
     liGoldragonWebpage.flake = false;
